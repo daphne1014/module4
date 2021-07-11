cb9047373fbff597cb04054da5c355d689d4450b
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-07-10 17:30:52 -0700

    add questions and anwers variable

---
+++ assets/js/style.js
@@ -0,0 +1,30 @@
+let questions = [
+    {
+        q: 'What is a pseudo-class?',
+        a: ['A CSS rule that contains no declarations',
+            'A CSS declaration that hides the element.',
+            'An element that has more than one class.',
+            'A CSS keyword to target an element\'s state.'] //this is the answer
+    },
+    {
+        q: 'What is an example of a pseudo-element?',
+        a: ['::before',
+            '::after',
+            '::first-letter',
+            'All of the above.'] //this is the answer
+    },
+    {
+        q: 'What does the z-index property do?',
+        a: ['Removes an element from the DOM.',
+            'Changes the stacking order of elements.', //this is the answer
+            'Changes the opacity of an element.',
+            'Forces an element to be positioned relatively.']
+    },
+    {
+        q: 'Which of the following is NOT a reason to validate a user\'s responses?',
+        a: ['Offers the user an opportunity to enter a correct response.',
+            'Reduces bogus answers getting stored in the database.',
+            'Improves the user experience.', //this is the answer
+            'Increases the overall quality of the user data.']
+    }
+];
